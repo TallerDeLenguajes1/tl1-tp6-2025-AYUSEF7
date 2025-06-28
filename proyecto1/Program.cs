b76c1edbad19c25cf5578b5e01e71c537f3589ee
“AYUSEF7” <yusefakd07@gmail.com>
2025-06-27 23:53:29 -0300

    Ejercicio 3

--- proyecto1/Program.cs
+++ proyecto1/Program.cs
@@ -33,3 +33,45 @@ if (int.TryParse(entrada, out numero)){                             //verifico s
 else{
     Console.WriteLine("La entrada no es un número válido.");
 }
+
+//Ejercicio 3
+
+Console.WriteLine("Ingrese un número: ");
+string entrada1 = Console.ReadLine();
+float numero3;
+
+if (float.TryParse(entrada1, out numero3)){
+    Console.WriteLine("\n--- Operaciones Matemáticas ---");
+
+    Console.WriteLine($"Valor absoluto: {Math.Abs(numero3)}");                      //valor absoluto
+    Console.WriteLine($"Cuadrado: {Math.Pow(numero3, 2)}");                         //potencia al cuadrado
+    if (numero3 >= 0)                                                               //raiz cuadrada
+        Console.WriteLine($"Raíz cuadrada: {Math.Sqrt(numero3)}");
+    else
+        Console.WriteLine("Raíz cuadrada: No definida para negativos");
+    Console.WriteLine($"Seno: {Math.Sin(numero3)}");                                //seno
+    Console.WriteLine($"Coseno: {Math.Cos(numero3)}");                              //coseno
+    Console.WriteLine($"Parte entera (truncate): {Math.Truncate(numero3)}");        //La parte entera de un tipo float.
+    
+} else {
+    Console.WriteLine("No ingresó un número válido.");
+    return;
+}
+
+Console.WriteLine("\nIngrese el primer número: ");
+string entrada2 = Console.ReadLine();
+float n1;
+
+Console.WriteLine("Ingrese el segundo número: ");
+string entrada3 = Console.ReadLine();
+float n2;
+
+
+if (float.TryParse(entrada2, out n1) && float.TryParse(entrada3, out n2)){          //control de que los 2 sean digitos
+    Console.WriteLine("\n--- Comparación ---");
+
+    Console.WriteLine($"Máximo: {Math.Max(n1, n2)}");
+    Console.WriteLine($"Mínimo: {Math.Min(n1, n2)}");
+} else {
+    Console.WriteLine("Uno o ambos valores no son válidos.");
+}
